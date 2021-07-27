c73fdd02cc8920488fe354a120d11b299ab40a87
HD-4Code <86689864+HD-4Code@users.noreply.github.com>
2021-07-27 05:28:05 +0200

    Update controller.rb

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,18 +1,23 @@
+require 'gossip'
 
 class ApplicationController < Sinatra::Base
-  get '/' do
-      erb :index
+    get '/' do
+      erb :index, locals: {gossips: Gossip.all}
     end
 
-  get '/gossips/new/' do
-    erb :new_gossip
-  end
+    get '/gossips/new/' do
+      erb :new_gossip
+    end
 
-  post '/gossips/new/' do
-    #"Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
-    Gossip.new(les_entrées_du_gossip).save
-  end
+    post '/gossips/new/' do
+      Gossip.new(params["gossip_author"],params["gossip_content"]).save
+      redirect '/'
+    end
 
+    get '/gossips/:id/' do 
+      erb :show, locals: {gossip: Gossip.find(params['id']), id: params['id']}
+      redirect '/'
+    end
 
-
+    
 end
